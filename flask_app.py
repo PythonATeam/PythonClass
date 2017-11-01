0c799338bb9849a28dd46039db6284e55c7dfcd2
csblu22 <perry.florida@hotmail.com>
2017-11-01 19:56:39 +0000

    FIRST DB COMMIT

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,30 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="tilted4",
+    password="grades777",
+    hostname="tilted4.mysql.pythonanywhere-services.com",
+    databasename="tilted4$default",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Games(db.Model):
+
+    __tablename__ = "games"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(4096), nullable = False)
+
 
 @app.route('/')
 def index():
