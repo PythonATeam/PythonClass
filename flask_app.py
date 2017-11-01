b904876a59df8f2f4f604ccd2a56a3deb29a4be5
csblu22 <perry.florida@hotmail.com>
2017-11-01 20:37:46 +0000

    Yay, it's all finished :-D

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
     username="tilted4",
     password="grades777",
     hostname="tilted4.mysql.pythonanywhere-services.com",
-    databasename="tilted4$default",
+    databasename="tilted4$comments",
 )
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
@@ -19,15 +19,29 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-class Games(db.Model):
+class Comment(db.Model):
 
-    __tablename__ = "games"
+    __tablename__ = "comments"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(4096), nullable = False)
+    content = db.Column(db.String(4096), nullable = False)
 
 
-@app.route('/')
+
+
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("main_page.html")
+    if request.method == "GET":
+        return render_template("main_page.html", comments=Comment.query.all())
+
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+
+
 
