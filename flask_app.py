0b0c5ac7f0104cddd2769a76551a3d9d0d7990aa
csblu22 <perry.florida@hotmail.com>
2017-11-28 21:06:36 +0000

    Register page updated with most if not all fields

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,7 @@ from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import current_user, login_required, login_user, LoginManager, logout_user, UserMixin
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -30,8 +30,13 @@ class User(UserMixin, db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(128))
-    password_hash = db.Column(db.String(128))
+    username = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    birth_date = db.Column(db.String(128), nullable=False)
+    last_name = db.Column(db.String(128))
+    first_name = db.Column(db.String(128))
+    comments = db.relationship('Comment')
 
 
     def check_password(self, password):
@@ -41,6 +46,9 @@ class User(UserMixin, db.Model):
     def get_id(self):
         return self.username
 
+    def __repr__(self):
+        return "<User(fname='%s', lname='%s', username='%s', email='%s', birth_date='%s')>" % (
+                self.first_name, self.last_name, self.username, self.email, self.birth_date)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -52,9 +60,10 @@ class Comment(db.Model):
     __tablename__ = "comments"
 
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(4096))
+    content = db.Column(db.String(4096), nullable=False)
     posted = db.Column(db.DateTime, default=datetime.now)
     commenter_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    #game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=True)
     commenter = db.relationship('User', foreign_keys=commenter_id)
 
 
@@ -85,6 +94,28 @@ def login():
         return render_template("login_page.html", error=True)
 
     login_user(user)
+    return redirect(url_for('index'))
+
+
+@app.route("/register/", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register_page.html", error=False)
+
+    user = load_user(request.form["username"])
+
+    if not user is None:
+        return render_template("register_page.html", error=True)
+
+    if request.form["password"] != request.form["pwconfirm"]:
+        return render_template("register_page.html", passfail=True)
+
+
+    newuser = User(username=request.form["username"], password_hash=generate_password_hash(request.form["password"]),
+    email=request.form["email"], birth_date=request.form["bday"], first_name=request.form["fname"], last_name=request.form["lname"])
+
+    db.session.add(newuser)
+    db.session.commit()
     return redirect(url_for('index'))
 
 
